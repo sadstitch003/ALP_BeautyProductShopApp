57b3efe61f48acbe456c9943d3ae049e3152d8bb
Christian Sad <christian.kartika1@gmail.com>
2022-06-01 16:23:04 +0700

    rename

--- ALP_BeautyProductShopApp/Program.cs
+++ ALP_BeautyProductShopApp/Program.cs
@@ -16,7 +16,7 @@ namespace ALP_BeautyProductShopApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new LoginForm());
         }
     }
 }
