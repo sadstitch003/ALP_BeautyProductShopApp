79450449230262b26f41c2fcc97e9d44d3a288f9
Christian Sad <christian.kartika1@gmail.com>
2022-06-08 01:52:16 +0700

    tes

--- ALP_BeautyProductShopApp/TransactionDeleted.Designer.cs
+++ ALP_BeautyProductShopApp/TransactionDeleted.Designer.cs
@@ -29,22 +29,125 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TransactionDeleted));
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pnlBorder = new System.Windows.Forms.Panel();
+            this.BtnExit = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblTransID = new System.Windows.Forms.Label();
+            this.pnlBorder.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.LightCoral;
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel4.Location = new System.Drawing.Point(5, 453);
+            this.panel4.Name = "panel4";
+            this.panel4.Padding = new System.Windows.Forms.Padding(0, 0, 0, 5);
+            this.panel4.Size = new System.Drawing.Size(785, 5);
+            this.panel4.TabIndex = 46;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.LightCoral;
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel3.Location = new System.Drawing.Point(790, 35);
+            this.panel3.Name = "panel3";
+            this.panel3.Padding = new System.Windows.Forms.Padding(0, 0, 0, 5);
+            this.panel3.Size = new System.Drawing.Size(5, 423);
+            this.panel3.TabIndex = 45;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.LightCoral;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 35);
+            this.panel2.Name = "panel2";
+            this.panel2.Padding = new System.Windows.Forms.Padding(0, 0, 0, 5);
+            this.panel2.Size = new System.Drawing.Size(5, 423);
+            this.panel2.TabIndex = 44;
+            // 
+            // pnlBorder
+            // 
+            this.pnlBorder.BackColor = System.Drawing.Color.LightCoral;
+            this.pnlBorder.Controls.Add(this.BtnExit);
+            this.pnlBorder.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlBorder.Location = new System.Drawing.Point(0, 0);
+            this.pnlBorder.Name = "pnlBorder";
+            this.pnlBorder.Padding = new System.Windows.Forms.Padding(0, 0, 0, 5);
+            this.pnlBorder.Size = new System.Drawing.Size(795, 35);
+            this.pnlBorder.TabIndex = 43;
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.BackColor = System.Drawing.Color.Transparent;
+            this.BtnExit.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BtnExit.BackgroundImage")));
+            this.BtnExit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.BtnExit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnExit.Dock = System.Windows.Forms.DockStyle.Right;
+            this.BtnExit.FlatAppearance.BorderSize = 0;
+            this.BtnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnExit.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnExit.Location = new System.Drawing.Point(735, 0);
+            this.BtnExit.Margin = new System.Windows.Forms.Padding(0);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(60, 30);
+            this.BtnExit.TabIndex = 6;
+            this.BtnExit.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.LightCoral;
+            this.panel1.Location = new System.Drawing.Point(33, 83);
+            this.panel1.Margin = new System.Windows.Forms.Padding(25, 3, 25, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(728, 1);
+            this.panel1.TabIndex = 49;
+            // 
+            // lblTransID
+            // 
+            this.lblTransID.AutoSize = true;
+            this.lblTransID.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTransID.Location = new System.Drawing.Point(31, 54);
+            this.lblTransID.Name = "lblTransID";
+            this.lblTransID.Size = new System.Drawing.Size(228, 21);
+            this.lblTransID.TabIndex = 48;
+            this.lblTransID.Text = "Deleted Transaction Data";
             // 
             // TransactionDeleted
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.MistyRose;
-            this.ClientSize = new System.Drawing.Size(792, 450);
+            this.ClientSize = new System.Drawing.Size(795, 458);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblTransID);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.pnlBorder);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "TransactionDeleted";
             this.Text = "TransactionDeleted";
+            this.pnlBorder.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel pnlBorder;
+        private System.Windows.Forms.Button BtnExit;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblTransID;
     }
 }
