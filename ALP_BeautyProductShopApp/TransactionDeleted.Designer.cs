fde851c1e59afc21f771e507b6e7e7afbe493b43
Christian Sad <christian.kartika1@gmail.com>
2022-06-08 23:59:36 +0700

    e

--- ALP_BeautyProductShopApp/TransactionDeleted.Designer.cs
+++ ALP_BeautyProductShopApp/TransactionDeleted.Designer.cs
@@ -170,6 +170,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "TransactionDeleted";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "TransactionDeleted";
             this.pnlBorder.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dgvDeletedData)).EndInit();
