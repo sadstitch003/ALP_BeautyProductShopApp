3878eb75cf63672a5dd07cc89fef4e9ddf6a1a1b
Michell Christanto <mchristanto01@student.ciputra.ac.id>
2022-06-12 19:51:21 +0700

    tryc

--- ALP_BeautyProductShopApp/Staff.cs
+++ ALP_BeautyProductShopApp/Staff.cs
@@ -28,55 +28,76 @@ namespace ALP_BeautyProductShopApp
         int staffke;
         private void Staff_Load(object sender, EventArgs e)
         {
-            
-            dtStaff = new DataTable();
-            sqlQuery = "select staff_id, staff_name, staff_password, staff_position, staff_phone, staff_dob from staff where status_del ='0';";
-            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
-            sqlAdapter = new MySqlDataAdapter(sqlCommand);
-            sqlAdapter.Fill(dtStaff);
-            dgv_Staff.DataSource = dtStaff;
+            try
+            {
+                dtStaff = new DataTable();
+                sqlQuery = "select staff_id, staff_name, staff_password, staff_position, staff_phone, staff_dob from staff where status_del ='0';";
+                sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+                sqlAdapter = new MySqlDataAdapter(sqlCommand);
+                sqlAdapter.Fill(dtStaff);
+                dgv_Staff.DataSource = dtStaff;
 
-            sqlQuery = "select distinct staff_position from staff;";
-            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
-            sqlAdapter = new MySqlDataAdapter(sqlCommand);
-            sqlAdapter.Fill(dtPosition);
-            cBox_Position.DataSource = dtPosition;
-            cBox_Position.DisplayMember = "staff_position";
-           
-            staffid = dtStaff.Rows[0][0].ToString();
+                sqlQuery = "select distinct staff_position from staff;";
+                sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+                sqlAdapter = new MySqlDataAdapter(sqlCommand);
+                sqlAdapter.Fill(dtPosition);
+                cBox_Position.DataSource = dtPosition;
+                cBox_Position.DisplayMember = "staff_position";
+
+                staffid = dtStaff.Rows[0][0].ToString();
+            }
+            catch
+            {
+
+            }
 
         }
         private void dgv_Staff_CellEnter(object sender, DataGridViewCellEventArgs e)
         {
-            tBox_StaffID.Text = dgv_Staff.CurrentRow.Cells[0].Value.ToString();
-            tBox_StaffName.Text = dgv_Staff.CurrentRow.Cells[1].Value.ToString();
-            tBox_Password.Text = dgv_Staff.CurrentRow.Cells[2].Value.ToString();
-            cBox_Position.Text = dgv_Staff.CurrentRow.Cells[3].Value.ToString();
-            tBox_Phone.Text = dgv_Staff.CurrentRow.Cells[4].Value.ToString();
-            dTP_dob.Text = dgv_Staff.CurrentRow.Cells[5].Value.ToString();
+            try
+            {
+                tBox_StaffID.Text = dgv_Staff.CurrentRow.Cells[0].Value.ToString();
+                tBox_StaffName.Text = dgv_Staff.CurrentRow.Cells[1].Value.ToString();
+                tBox_Password.Text = dgv_Staff.CurrentRow.Cells[2].Value.ToString();
+                cBox_Position.Text = dgv_Staff.CurrentRow.Cells[3].Value.ToString();
+                tBox_Phone.Text = dgv_Staff.CurrentRow.Cells[4].Value.ToString();
+                dTP_dob.Text = dgv_Staff.CurrentRow.Cells[5].Value.ToString();
+            }
+            catch
+            {
+
+            }
         }
        
         private void btn_Save_Click(object sender, EventArgs e)
         {
-            dtPosition = new DataTable();
-            sqlQuery = "select count(staff_id) from staff where staff_id = '" + tBox_StaffID.Text + "';";
-            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
-            sqlConnect.Open();
-            int temp = Convert.ToInt32(sqlCommand.ExecuteScalar().ToString());
-            if (temp > 0)
+            try
             {
-                MessageBox.Show("data sudah ada");
-                tBox_StaffID.Text = null;
-            }
-            else
-            {
-                sqlQuery = "insert into staff values ('" + tBox_StaffID.Text + "','" + tBox_StaffName.Text + "','" + tBox_Password.Text + "','" + cBox_Position.Text + "','" + tBox_Phone.Text + "','" + dTP_dob.Value.ToString("yyyyMMdd") + "','0');";
+                dtPosition = new DataTable();
+                sqlQuery = "select count(staff_id) from staff where staff_id = '" + tBox_StaffID.Text + "';";
                 sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
-                sqlCommand.ExecuteNonQuery();
-                MessageBox.Show("Data telah tersimpan");
-                Staff_Load(sender, e);
+                sqlConnect.Open();
+                int temp = Convert.ToInt32(sqlCommand.ExecuteScalar().ToString());
+                if (temp > 0)
+                {
+                    MessageBox.Show("data sudah ada");
+                    tBox_StaffID.Text = null;
+                }
+                else
+                {
+                    sqlQuery = "insert into staff values ('" + tBox_StaffID.Text + "','" + tBox_StaffName.Text + "','" + tBox_Password.Text + "','" + cBox_Position.Text + "','" + tBox_Phone.Text + "','" + dTP_dob.Value.ToString("yyyyMMdd") + "','0');";
+                    sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+                    sqlCommand.ExecuteNonQuery();
+                    MessageBox.Show("Data telah tersimpan");
+                    Staff_Load(sender, e);
+                }
+            }
+            catch
+            {
+
             }
             sqlConnect.Close();
+            
         }
         private void btn_Update_Click(object sender, EventArgs e)
         {
@@ -102,66 +123,107 @@ namespace ALP_BeautyProductShopApp
 
         private void btn_Delete_Click(object sender, EventArgs e)
         {
-            dtPosition = new DataTable();
-            sqlQuery = "UPDATE staff SET status_del = '1'WHERE staff_id = '" + tBox_StaffID.Text + "';";
-            sqlConnect.Open();
-            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
-            sqlCommand.ExecuteNonQuery();
-            sqlConnect.Close();
-            MessageBox.Show("Data telah terhapus");
-            Staff_Load(sender, e);
+            try
+            {
+                dtPosition = new DataTable();
+                sqlQuery = "UPDATE staff SET status_del = '1'WHERE staff_id = '" + tBox_StaffID.Text + "';";
+                sqlConnect.Open();
+                sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+                sqlCommand.ExecuteNonQuery();
+                sqlConnect.Close();
+                MessageBox.Show("Data telah terhapus");
+                Staff_Load(sender, e);
+            }
+            catch
+            {
+
+            }
         }
 
         private void btnViewSales_Click(object sender, EventArgs e)
         {
+            try
+            {
+                StaffSales sales = new StaffSales();
+                sales.MdiParent = this.ParentForm;
+                sales.Dock = DockStyle.Fill;
+                sales.Show();
+            }
+            catch
+            {
 
-            StaffSales sales = new StaffSales();
-            sales.MdiParent = this.ParentForm;
-            sales.Dock = DockStyle.Fill;
-            sales.Show();
+            }
         }
 
         private void dgv_Staff_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            staffke = dgv_Staff.CurrentCell.RowIndex;
-            staffid = dtStaff.Rows[staffke][0].ToString();
+            try
+            {
+                staffke = dgv_Staff.CurrentCell.RowIndex;
+                staffid = dtStaff.Rows[staffke][0].ToString();
+            }
+            catch
+            {
+
+            }
         }
 
         private void btn_clear_Click(object sender, EventArgs e)
         {
-            tBox_StaffID.Clear();
-            tBox_StaffName.Clear();
-            tBox_Password.Clear();
-            tBox_Phone.Clear();
+            try
+            {
+                tBox_StaffID.Clear();
+                tBox_StaffName.Clear();
+                tBox_Password.Clear();
+                tBox_Phone.Clear();
+            }
+            catch
+            {
+
+            }
         }
 
         private void btn_Search_Click(object sender, EventArgs e)
         {
-            dtStaff = new DataTable();
-            sqlQuery = "select staff_id, staff_name, staff_password, staff_position, staff_phone, staff_dob from staff where status_del ='0'and (staff_name like '%" + tBox_Search.Text + "%' or staff_id like '%" + tBox_Search.Text + "%' or staff_phone like '%"+tBox_Search.Text+"%');";
-            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
-            sqlAdapter = new MySqlDataAdapter(sqlCommand);
-            sqlAdapter.Fill(dtStaff);
-            sqlAdapter.Fill(dtPosition);
-            dgv_Staff.DataSource = dtStaff;
-            cBox_Position.DataSource = dtPosition;
-            cBox_Position.DisplayMember = "staff_position";
-            cBox_Position.ValueMember = "staff_id";
-            if (dgv_Staff.Rows.Count > 0)
+            try
             {
-                staffid = dtStaff.Rows[0][0].ToString();
+                dtStaff = new DataTable();
+                sqlQuery = "select staff_id, staff_name, staff_password, staff_position, staff_phone, staff_dob from staff where status_del ='0'and (staff_name like '%" + tBox_Search.Text + "%' or staff_id like '%" + tBox_Search.Text + "%' or staff_phone like '%" + tBox_Search.Text + "%');";
+                sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+                sqlAdapter = new MySqlDataAdapter(sqlCommand);
+                sqlAdapter.Fill(dtStaff);
+                sqlAdapter.Fill(dtPosition);
+                dgv_Staff.DataSource = dtStaff;
+                cBox_Position.DataSource = dtPosition;
+                cBox_Position.DisplayMember = "staff_position";
+                cBox_Position.ValueMember = "staff_id";
+                if (dgv_Staff.Rows.Count > 0)
+                {
+                    staffid = dtStaff.Rows[0][0].ToString();
+                }
+                else
+                {
+                    MessageBox.Show("ulangi");
+                }
             }
-            else
+            catch
             {
-                MessageBox.Show("ulangi");
+
             }
         }
 
         private void btn_refresh_Click(object sender, EventArgs e)
         {
-            dtPosition = new DataTable();
-            Staff_Load(sender, e);
-            tBox_Search.Text = "";
+            try
+            {
+                dtPosition = new DataTable();
+                Staff_Load(sender, e);
+                tBox_Search.Text = "";
+            }
+            catch
+            {
+
+            }
         }
     }
 }
